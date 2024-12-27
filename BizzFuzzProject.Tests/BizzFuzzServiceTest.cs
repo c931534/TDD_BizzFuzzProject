86d7e676abaf5dcefb6de9e7b7ccb5237abec8b7
etta.huang <etta.huang@ailabpte.co>
2024-12-27 23:54:12 +0800

    add return Fuzz Test

--- BizzFuzzProject.Tests/BizzFuzzServiceTest.cs
+++ BizzFuzzProject.Tests/BizzFuzzServiceTest.cs
@@ -31,4 +31,13 @@ public class BizzFuzzServiceTest
     {
         Assert.That(_service.Query(3), Is.EqualTo("Bizz"));
     }
+    
+    /// <summary>
+    /// 可以被3整除地要回傳bizz
+    /// </summary>
+    [Test]
+    public void Return_Fuzz()
+    {
+        Assert.That(_service.Query(5), Is.EqualTo("Fuzz"));
+    }
 }
