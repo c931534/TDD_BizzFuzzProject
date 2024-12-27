4b86cd5e5511bde043835f8c6fbca27c6d2ec8f8
etta.huang <etta.huang@ailabpte.co>
2024-12-28 00:10:10 +0800

    refactor

--- BizzFuzzProject/BizzFuzzService.cs
+++ BizzFuzzProject/BizzFuzzService.cs
@@ -4,12 +4,17 @@ public class BizzFuzzService
 {
     public string Query(int number)
     {
-        if ((number.ToString().Contains("3") || number % 3 == 0) && number % 5 != 0)
+        if (IsBizz(number) && number % 5 != 0)
             return "Bizz";
-        if (number % 5 == 0 && number % 3 == 0)
+        if (number % 5 == 0 && IsBizz(number))
             return "BizzFuzz";
         if (number % 5 == 0)
             return "Fuzz";
         return number.ToString();
+    }
+
+    private bool IsBizz(int number)
+    {
+        return number.ToString().Contains("3") || number % 3 == 0;
     }
 }
